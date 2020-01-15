0c413878ee2c826ca7f066eeb91bd15f1d920b76
leelee92 <55240847+leelee92@users.noreply.github.com>
2020-01-15 23:39:16 +0100

    ajout FirstComponent

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1> Titre 1 </h1>
+        <h2> Titre 2 </h2>
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+
+</style>
+
